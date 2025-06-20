59d21319a755ec5b4e40a4b8916b17387d8f4ea8
ChenJH <3272689875@qq.com>
2025-06-20 12:27:53 +0800

    流式输出改善以及用户个性化回答

---
+++ src/main/java/com/cqupt/java/ai/langchain4j/service/PromptService.java
@@ -0,0 +1,4 @@
+package com.cqupt.java.ai.langchain4j.service;
+
+public interface PromptService {
+}
---
+++ src/main/java/com/cqupt/java/ai/langchain4j/service/impl/PromptServiceImpl.java
@@ -0,0 +1,4 @@
+package com.cqupt.java.ai.langchain4j.service.impl;
+
+public class PromptServiceImpl {
+}
